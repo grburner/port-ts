c4c43ada3bd1e2082d35b0f4303343630eeb5030
Gary Burner <grburner@gmail.com>
2020-12-26 19:58:05 -0500

    updated image picker and description to be in sync

--- src/components/ProjectRenderer/index.tsx
+++ src/components/ProjectRenderer/index.tsx
@@ -12,20 +12,20 @@ const ProjectRenderer: React.FC<Props> = (props) => {
 
   return (
     <div className={`project-images`}>
-      <div onClick={() => props.onChange(0)}>
+      <div onClick={() => props.onChange(projectArrangement[0])}>
         <ProjectImage image={data.projects[projectArrangement[0]].image} />
       </div>
       {width < 700 ? (
         ""
       ) : (
-        <div onClick={() => props.onChange(1)}>
+        <div onClick={() => props.onChange(projectArrangement[1])}>
           <ProjectImage image={data.projects[projectArrangement[1]].image} />
         </div>
       )}
       {width < 1300 ? (
         ""
       ) : (
-        <div onClick={() => props.onChange(2)}>
+        <div onClick={() => props.onChange(projectArrangement[2])}>
           <ProjectImage image={data.projects[projectArrangement[2]].image} />
         </div>
       )}
